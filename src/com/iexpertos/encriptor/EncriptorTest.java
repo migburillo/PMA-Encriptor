3e73adec0ef4f39a57720ff0e55a72c305077d7a
miguel <miguel@blackin.(none)>
2013-04-04 11:57:45 +0200

    Tests for invalid input. Tests naming improved

--- src/com/iexpertos/encriptor/EncriptorTest.java
+++ src/com/iexpertos/encriptor/EncriptorTest.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.security.InvalidParameterException;
+
 public class EncriptorTest {
 	Encriptor encriptor;
 
@@ -20,42 +22,42 @@ public class EncriptorTest {
 	}
 
 	@Test
-	public void testCryptWordString() {
+	public void crypt_word_hola() {
 		String expected = "jqnc";
 		String response = encriptor.cryptWord("hola");
 		assertThat(expected, equalTo(response));
 	}
 
 	@Test
-	public void testCryptWordStringString() {
+	public void crypt_word_hola_one_char_only() {
 		String expected = "hqla";
 		String response = encriptor.cryptWord("hola", "o");
 		assertThat(expected, equalTo(response));
 	}
 
 	@Test
-	public void testCryptWordToNumbers() {
+	public void crypt_word_hola_to_numbers() {
 		String expected = "10611311099";
 		String response = encriptor.cryptWordToNumbers("hola");
 		assertThat(expected, equalTo(response));
 	}
 
 	@Test
-	public void testCryptSentence() {
+	public void crypt_sentence() {
 		String expected = "jqnc.\"swg\"vcn\"guvcuA";
 		String response = encriptor.cryptSentence("hola, que tal estas?");
 		assertThat(expected, equalTo(response));
 	}
 
 	@Test
-	public void testGetWords() {
-		String[] expected = {"hola,", "que", "tal", "estas?"};
+	public void get_words() {
+		String[] expected = { "hola,", "que", "tal", "estas?" };
 		String[] response = encriptor.getWords("hola, que tal estas?");
 		assertThat(expected, equalTo(response));
 	}
 
 	@Test
-	public void testPrintWords() {
+	public void print_words() {
 		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
 
@@ -64,4 +66,18 @@ public class EncriptorTest {
 		assertThat(expected, equalTo(outContent.toString()));
 	}
 
+	@Test(expected = InvalidParameterException.class)
+	public void crypt_word_invalid_input() {
+		encriptor.cryptWord(" ");
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void crypt_word_hola_to_numbers_invalid_input() {
+		encriptor.cryptWord(" ");
+	}
+
+	@Test(expected = InvalidParameterException.class)
+	public void crypt_word_hola_one_char_only_invalid_input() {
+		encriptor.cryptWord(" ", "a");
+	}
 }
